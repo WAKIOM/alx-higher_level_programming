8423e00d57ce0330b8b428d3856d4a7b04c10de4
WAKIOM <agnessmanga@outlook.com>
2023-05-09 19:48:45 +0300

    uppercase

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -2,9 +2,9 @@
 def uppercase(str):
     string = ''
     for s in str:
-        if ord(s) >= 97 and ord(s) <=122:
+        if ord(s) >= 97 and ord(s) <= 122:
             s = chr(ord(s)-32)
             string += s
         else:
-            string += s 
+            string += s
     print("{:s}\n".format(string), end='')
