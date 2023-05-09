d49c52a254ee74e7b90618afb5a24945aca67a37
WAKIOM <agnessmanga@outlook.com>
2023-05-09 17:08:02 +0300

    double digits

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for num in range(0, 100, 1):
-    if num == 99:
+    if num==99:
         print("{}".format(num))
-    else:        
-        print("{:02d},".format(num), end = ' ' )
+    else:
+        print("{:02d},".format(num), end=' ')
