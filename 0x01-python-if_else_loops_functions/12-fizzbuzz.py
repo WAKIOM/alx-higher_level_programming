d3dbc5789f2c8fc175eebf0014ff5406d14a7711
WAKIOM <agnessmanga@outlook.com>
2023-05-09 21:17:24 +0300

    fizz buzz

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def fizzbuzz():
     for num in range(1, 101, 1):
-        if num % 3 == 0  and num % 5 == 0:
+        if num % 3 == 0 and num % 5 == 0:
             print("FizzBuzz", end=' ')
         elif num % 5 == 0:
             if num == 100:
