aa736eb134a854fc58f26e4a38db270f47f62f9f
WAKIOM <agnessmanga@outlook.com>
2023-05-12 11:15:41 +0300

    calculator

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+if __name__ == '__main__':
+    from calculator_1 import add, sub, mul, div
+    from sys import argv, stderr, exit
+    l = len(argv)
+    if l == 4:
+        a = int(argv[1])
+        operator = argv[2]
+        b = int(argv[3])
+        if operator == '+':
+            print("{} {} {} = {}".format(a, operator, b, add(a, b)))
+        elif operator == '-':
+            print("{} {} {} = {}".format(a, operator, b, sub(a, b)))
+        elif operator == '*':
+            print("{} {} {} = {}".format(a, operator, b, mul(a, b)))
+        elif operator == '/':
+            print("{} {} {} = {}".format(a, operator, b, div(a, b)))
+        else:
+            stderr.write("Unknown operator. Available operators: +, -, * and /\n")
+            exit(1)
+    else:
+        stderr.write("Usage: ./100-my_calculator.py <a> <operator> <b>\n")
+        exit(1)
