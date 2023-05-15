9b9df659b260765e873e4096394c33ed1ad96c74
WAKIOM <agnessmanga@outlook.com>
2023-05-15 19:44:58 +0300

    correct error

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    tuple_s = (len(sentence), sentence[0])
+    if sentence:
+        tuple_s = (len(sentence), sentence[0])
+    else:
+        tuple_s = (0, None)
     return tuple_s
