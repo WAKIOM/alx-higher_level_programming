4ca6c1e2c8134cb0fe0b36e9a1353a7a1f184b99
WAKIOM <agnessmanga@outlook.com>
2023-05-29 17:45:27 +0300

    correction

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -6,6 +6,6 @@ def safe_print_list(my_list=[], x=0):
             print(f"{item}", end="")
             nums += 1
         except IndexError:
-            pass
+            break
     print()
     return nums
