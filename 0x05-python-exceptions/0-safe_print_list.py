014742fe11f1408771f7df1ee564bb0765bc5d0d
WAKIOM <agnessmanga@outlook.com>
2023-05-29 17:48:00 +0300

    correction

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,9 +1,9 @@
 #!usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     nums = 0
-    for item in my_list:
+    for i in range(x):
         try:
-            print(f"{item}", end="")
+            print("{}".format(my_list[i]), end="")
             nums += 1
         except IndexError:
             break
