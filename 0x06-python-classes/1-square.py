b3a2c704fac0679904c6e1ca8c3a800163879188
WAKIOM <agnessmanga@outlook.com>
2023-05-30 18:12:12 +0300

    square instance

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" Class square that defines a square"""
+
+
+class Square:
+    """
+    Square class size
+
+
+    Attributes:
+        __size(int): Size of the square
+
+    Methods:
+        __init__(self, size): Initializes a Square instance
+    """
+    def __init__(self, size):
+        self.__size = size
