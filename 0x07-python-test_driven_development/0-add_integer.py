eeb0862b0aab8d42f8dea50726ba908d5ffd087b
WAKIOM <agnessmanga@outlook.com>
2023-10-26 14:22:06 +0300

    add files

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+"""
+This module contains a function that adds two integers
+a: first argument
+b: second argument
+"""
+def add_integer(a, b=98):
+    """
+    Function adds two integers
+    """
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+    else:
+        a, b = int(a), int(b)
+    return a + b
+
