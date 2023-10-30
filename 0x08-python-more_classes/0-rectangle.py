a52f127dd9d48cdcca57ada32e9078e5a22495f8
WAKIOM <agnessmanga@outlook.com>
2023-10-30 21:58:26 +0300

    add file

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+'''
+Defines a rectangle
+'''
+
+
+class Rectangle:
+    """
+    This class represents a square.
+    A square of equal side lengths for creating square objects
+
+
+    Attributes:
+        None
+
+    Methods:
+        None
+    """
+    pass
