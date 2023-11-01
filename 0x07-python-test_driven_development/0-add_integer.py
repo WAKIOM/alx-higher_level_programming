6d875f407717f3a659aefcc50f4215adfbae24a1
WAKIOM <agnessmanga@outlook.com>
2023-11-01 12:28:18 +0300

    modify code

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -8,14 +8,12 @@ This module contains a function that adds two integers
 def add_integer(a, b=98):
     """
     Function adds two integers
-    args: a(int) first argument
+    args:
+          a(int) first argument
           b(int) second argument. if not provided, default = 98
     """
-    if not isinstance(a, (int, float)) or a is None:
+    if a is None or not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
-    if not isinstance(b, (int, float)) or b is None:
+    if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
-    result = a + b
-    if result == float('inf') or result == -float('inf'):
-        return 89
     return int(a) + int(b)
