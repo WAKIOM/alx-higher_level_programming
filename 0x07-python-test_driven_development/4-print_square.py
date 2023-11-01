c441a66b2f2910a051e239af5f17997a2ed16890
WAKIOM <agnessmanga@outlook.com>
2023-11-01 10:33:02 +0300

    add files

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+
+
+def print_square(size):
+    """
+    Print a square of a given size using '#' characters.
+
+    Parameters:
+        size (int): The size of the square. It must be a non-negative integer.
+
+    Raises:
+        TypeError: If `size` is not an integer or is a negative float.
+        ValueError: If `size` is a negative integer.
+
+    Returns:
+        None
+
+    Example:
+        >>> print_square(5)
+        #####
+        #####
+        #####
+        #####
+        #####
+    """
+    if isinstance(size, float) and size < 0:
+        raise TypeError("size must be an integer")
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    for i in range(size):
+        print("#" * size, end="\n")
