5810c2a57d6fb20ce65ebcfd66ec80e850cca5d8
WAKIOM <agnessmanga@outlook.com>
2023-11-06 22:21:21 +0300

    addd file

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+BaseGeometry module
+"""
+
+
+class BaseGeometry:
+    """
+    class contains one method
+    methods: area
+    """
+    def area(self):
+        """
+        a method that raises an exception
+        """
+        raise Exception("area() is not implemented")
