ae0753870991ae6d02d1c04a9b1cbe504a247508
WAKIOM <agnessmanga@outlook.com>
2023-11-07 13:53:11 +0300

    try fixing issue

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
-import json
 
 """
 Module contains a function that returns the JSON
-representation of an object (string)
+representation of an object (string))
+"""
+import json
+
+"""
+json module
 """
 
 
