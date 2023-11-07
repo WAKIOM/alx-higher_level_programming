f6c88fcaa50528c582d3473eace57143a73a3205
WAKIOM <agnessmanga@outlook.com>
2023-11-07 13:41:54 +0300

    make changes to 3-to_json_str

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -6,8 +6,13 @@ Module contains a function that returns the JSON
 representation of an object (string)
 """
 
+
 def to_json_string(my_obj):
     """
     Function returns the JSON representation of a string
+    args:
+    my_obj: object to be converted
+    returns:
+    str: The JSON representation of the object as a string.
     """
     return json.dumps(my_obj)
