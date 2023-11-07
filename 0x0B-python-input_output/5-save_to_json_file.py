fbe6ac569c7fda0bdad3b8fa3b4a24bbeabe785c
WAKIOM <agnessmanga@outlook.com>
2023-11-07 16:12:45 +0300

    adit

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -14,6 +14,6 @@ def save_to_json_file(my_obj, filename):
     a  JSON representation
     takes two arguments
     """
-    with open(filename, 'a') as f:
+    with open(filename, 'w') as f:
 
         json.dump(my_obj, f)
