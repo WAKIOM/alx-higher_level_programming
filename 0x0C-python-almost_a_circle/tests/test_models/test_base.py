2152eaa65fa13a446c7a59f57857d47ae7b732da
WAKIOM <agnessmanga@gmail.com>
2023-11-13 18:20:55 +0300

    add tests

---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -0,0 +1,75 @@
+#!/usr/bin/python3
+
+"""
+Unit tests for the Base class.
+
+These tests cover various scenarios for initializing instances of
+the Base class with different types of 'id' parameters.
+
+Tested scenarios include:
+- Initializing with an integer 'id'.
+- Initializing with a float 'id'.
+- Initializing with a string 'id'.
+- Initializing with a list 'id'.
+- Initializing with a tuple 'id'.
+- Initializing with a dictionary 'id'.
+- Initializing with a boolean 'id'.
+- Initializing multiple instances to ensure unique 'id' values.
+- Checking the value of the class variable __nb_objects
+after multiple instances are created.
+
+"""
+
+import unittest
+from models.base import Base
+
+class TestBaseClass(unittest.TestCase):
+
+    def test_init_with_int_id(self):
+        """Test initializing with an integer 'id'."""
+        obj = Base(id=42)
+        self.assertEqual(obj.id, 42)
+
+    def test_init_with_float_id(self):
+        """Test initializing with a float 'id'."""
+        obj = Base(id=3.14)
+        self.assertEqual(obj.id, 3.14)
+
+    def test_init_with_string_id(self):
+        """Test initializing with a string 'id'."""
+        obj = Base(id="abc")
+        self.assertEqual(obj.id, "abc")
+
+    def test_init_with_list_id(self):
+        """Test initializing with a list 'id'."""
+        custom_list = [1, 2, 3]
+        obj = Base(id=custom_list)
+        self.assertEqual(obj.id, custom_list)
+
+    def test_init_with_tuple_id(self):
+        """Test initializing with a tuple 'id'."""
+        custom_tuple = (4, 5, 6)
+        obj = Base(id=custom_tuple)
+        self.assertEqual(obj.id, custom_tuple)
+
+    def test_init_with_dict_id(self):
+        """Test initializing with a dictionary 'id'."""
+        custom_dict = {"key": "value"}
+        obj = Base(id=custom_dict)
+        self.assertEqual(obj.id, custom_dict)
+
+    def test_init_with_bool_id(self):
+        """Test initializing with a boolean 'id'."""
+        obj_true = Base(id=True)
+        obj_false = Base(id=False)
+        self.assertEqual(obj_true.id, True)
+        self.assertEqual(obj_false.id, False)
+
+    def test_init_with_multiple_instances(self):
+        """Test initializing multiple instances to ensure unique 'id' values."""
+        obj1 = Base()
+        obj2 = Base()
+        self.assertNotEqual(obj1.id, obj2.id)
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -0,0 +1,100 @@
+import unittest
+import sys
+import io
+from models.rectangle import Rectangle
+""" test cases for rectangle class """
+
+
+class TestRectangle(unittest.TestCase):
+
+    def test_init_with_valid_arguments(self):
+        """Test initializing Rectangle with valid arguments."""
+        rect = Rectangle(width=10, height=20, x=5, y=7, id=1)
+        self.assertEqual(rect.width, 10)
+        self.assertEqual(rect.height, 20)
+        self.assertEqual(rect.x, 5)
+        self.assertEqual(rect.y, 7)
+        self.assertEqual(rect.id, 1)
+
+    def test_init_with_invalid_width(self):
+        """Test initializing Rectangle with invalid width."""
+        with self.assertRaises(TypeError):
+            Rectangle(width="invalid", height=20, x=5, y=7, id=1)
+
+    def test_init_with_invalid_height(self):
+        """Test initializing Rectangle with invalid height."""
+        with self.assertRaises(TypeError):
+            Rectangle(width=10, height="invalid", x=5, y=7, id=1)
+
+    def test_init_with_negative_width(self):
+        """Test initializing Rectangle with negative width."""
+        with self.assertRaises(ValueError):
+            Rectangle(width=-10, height=20, x=5, y=7, id=1)
+
+    def test_init_with_negative_height(self):
+        """Test initializing Rectangle with negative height."""
+        with self.assertRaises(ValueError):
+            Rectangle(width=10, height=-20, x=5, y=7, id=1)
+
+    def test_init_with_invalid_x(self):
+        """Test initializing Rectangle with invalid x."""
+        with self.assertRaises(TypeError):
+            Rectangle(width=10, height=20, x="invalid", y=7, id=1)
+
+    def test_init_with_negative_x(self):
+        """Test initializing Rectangle with negative x."""
+        with self.assertRaises(ValueError):
+            Rectangle(width=10, height=20, x=-5, y=7, id=1)
+
+    def test_init_with_invalid_y(self):
+        """Test initializing Rectangle with invalid y."""
+        with self.assertRaises(TypeError):
+            Rectangle(width=10, height=20, x=5, y="invalid", id=1)
+
+    def test_init_with_negative_y(self):
+        """Test initializing Rectangle with negative y."""
+        with self.assertRaises(ValueError):
+            Rectangle(width=10, height=20, x=5, y=-7, id=1)
+
+    def test_str_representation(self):
+        """Test the __str__ representation of Rectangle."""
+        rect = Rectangle(width=10, height=20, x=5, y=7, id=1)
+        self.assertEqual(str(rect), "[Rectangle] (1) 5/7 - 10/20")
+
+    def test_area_calculation(self):
+        """Test calculating the area of Rectangle."""
+        rect = Rectangle(width=10, height=20, x=5, y=7, id=1)
+        self.assertEqual(rect.area(), 200)
+
+    def test_display_method(self):
+        """Test the display method of Rectangle."""
+        rect = Rectangle(width=3, height=2, x=1, y=1, id=1)
+        expected_output = "\n ###\n ###\n"
+        captured_output = io.StringIO()
+        sys.stdout = captured_output
+        rect.display()
+        sys.stdout = sys.__stdout__
+        self.assertEqual(captured_output.getvalue(), expected_output)
+
+    def test_update_method_with_args(self):
+        """Test the update method of Rectangle with arguments."""
+        rect = Rectangle(width=10, height=20, x=5, y=7, id=1)
+        rect.update(2, 15, 25, 8, 10)
+        self.assertEqual(rect.id, 2)
+        self.assertEqual(rect.width, 15)
+        self.assertEqual(rect.height, 25)
+        self.assertEqual(rect.x, 8)
+        self.assertEqual(rect.y, 10)
+
+    def test_update_method_with_kwargs(self):
+        """Test the update method of Rectangle with keyword arguments."""
+        rect = Rectangle(width=10, height=20, x=5, y=7, id=1)
+        rect.update(id=2, width=15, height=25, x=8, y=10)
+        self.assertEqual(rect.id, 2)
+        self.assertEqual(rect.width, 15)
+        self.assertEqual(rect.height, 25)
+        self.assertEqual(rect.x, 8)
+        self.assertEqual(rect.y, 10)
+
+if __name__ == '__main__':
+    unittest.main()
