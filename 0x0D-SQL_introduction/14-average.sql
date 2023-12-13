89db43db346d574606886f7b565362fe9f09b0be
WAKIOM <agnessmanga@outlook.com>
2023-12-13 05:24:55 -0800

    add file

---
+++ 0x0D-SQL_introduction/14-average.sql
@@ -0,0 +1,3 @@
+ -- get average score
+SELECT AVG(score) AS average
+FROM second_table;
---
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -0,0 +1,5 @@
+-- grouping data
+SELECT score, COUNT(score) AS number
+FROM second_table
+GROUP BY score
+ORDER BY score DESC;
